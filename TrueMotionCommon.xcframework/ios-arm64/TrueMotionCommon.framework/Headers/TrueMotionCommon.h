a481f1a8ab7ff8d2d3620b7724f24389fa2f157f
Adam Horvath <adam.horvath@gotruemotion.com>
2020-04-27 14:04:40 +0200

    Add xcframework

---
+++ TrueMotionCommon.xcframework/ios-arm64/TrueMotionCommon.framework/Headers/TrueMotionCommon.h
@@ -0,0 +1,13 @@
+// Copyright TrueMotion Inc. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TrueMotionCommon.
+FOUNDATION_EXPORT double TrueMotionCommonVersionNumber;
+
+//! Project version string for TrueMotionCommon.
+FOUNDATION_EXPORT const unsigned char TrueMotionCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrueMotionCommon/PublicHeader.h>
+
+
